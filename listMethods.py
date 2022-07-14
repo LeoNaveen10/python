b1e24bf61b3570042f310382965b9b1f7bfd6298
Naveen Sundar <naveensundar313@gmail.com>
2022-07-14 11:15:42 +0530

    first commit

---
+++ listMethods.py
@@ -0,0 +1,16 @@
+
+
+# append() -- adds Element to the end
+# pop() -- remove from end
+# arr2=arr1.copy() -- copy one list to another
+# 50 in arr  -- in operator give boolean if present or not
+# arr.index(4) - gives the index of no:4  in list
+# arr.sort()  -- sort the list
+# arr.reverse() - reverse the list
+# arr.count(3) -- returns  the no of 3's present in the list
+# arr.clear() -- clears the list
+# ar.remove(num) -- removes the particular num from the list
+
+
+# exercise to remove the duplicate no from the list
+arr = [1,6,7,1,6,5,4,7];
